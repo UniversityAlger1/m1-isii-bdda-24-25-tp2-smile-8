fe402b64f691926ec95daf643f0080899e80a418
smile-8 <rbesma66@gmail.com>
2025-04-29 20:29:35 +0100

    Update 3_3_allChercheurs.sql

--- 3_3_allChercheurs.sql
+++ 3_3_allChercheurs.sql
@@ -1,1 +1,10 @@
+-- Use LabDatabase Database
+USE LabDatabase;
+GO
+
+-- Display all Chercheur (NumCh, NomCh) of laboratoire that have CodeLab = '0001'
+SELECT NumCh, NomCh
+FROM Chercheur
+WHERE CodeLab = '0001';
+GO
 
