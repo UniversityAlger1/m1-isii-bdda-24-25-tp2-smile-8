3e454bbe70c36c1a7c012a0442745f8cc8aa10e9
smile-8 <rbesma66@gmail.com>
2025-04-29 20:29:12 +0100

    Update 3_2_recent.sql

--- 3_2_recent.sql
+++ 3_2_recent.sql
@@ -1,1 +1,13 @@
+-- Use LabDatabase Database
+USE LabDatabase;
+GO
+
+-- Display the name (NomLab) of the most recently created Laboratoire
+SELECT NomLab
+FROM Laboratoire
+WHERE Datcreation = (
+    SELECT MAX(Datcreation)
+    FROM Laboratoire
+);
+GO
 
