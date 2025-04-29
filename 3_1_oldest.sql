099461a52fe205bd20b52168d5a523ad7f93a708
smile-8 <rbesma66@gmail.com>
2025-04-29 20:28:52 +0100

    Update 3_1_oldest.sql

--- 3_1_oldest.sql
+++ 3_1_oldest.sql
@@ -1,1 +1,13 @@
+-- Use LabDatabase Database
+USE LabDatabase;
+GO
+
+-- Display the name (NomCh) of the oldest chercheur
+SELECT NomCh
+FROM Chercheur
+WHERE date_naissance = (
+    SELECT MIN(date_naissance)
+    FROM Chercheur
+);
+GO
 
